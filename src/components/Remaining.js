95483f08bb850f3c21e938c33ca2d683feb66f15
Ahmed Khan <emailahmedebadkhan@gmail.com>
2024-06-24 21:53:16 -0400

    up to expense total

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,25 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Remaining = () => {
+    //initialize an array and value based on context
+    const { expenses, budget } = useContext(AppContext);
+
+    //expenses has a member function called reduce
+    //it takes a callback lambda and initial value,
+    //this callback adds all the elements of the expenses array 
+    const totalExpenses = expenses.reduce((total, item) => 
+    {
+        return (total = total + item.cost);
+    }, 0);
+
+    //ternary operator to decide alert type
+    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+
+    return(
+        <div className={`alert ${alertType}`}>
+            <span>Remaining: £{budget - totalExpenses}</span>
+        </div>
+    );
+};
+export default Remaining;
